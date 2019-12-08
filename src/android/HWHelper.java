412edb082b42bb8dd679656738265a39b8144639
Dhaval Shah <dhavalkshah.2882@gmail.com>
2019-12-08 12:31:59 +0530

    Trying to integrate fb analytics

--- src/android/HWHelper.java
+++ src/android/HWHelper.java
@@ -11,9 +11,9 @@ import java.util.TimeZone;
 import android.provider.Settings;
 import android.util.Log;
 
-/**
- * This class echoes a string called from JavaScript.
- */
+import com.google.firebase.analytics.FirebaseAnalytics;
+import java.util.Iterator;
+import android.os.Bundle;
 public class HWHelper extends CordovaPlugin {
     public static final String TAG = "HWHelper";
     public static String platform;                            // Device OS
@@ -23,31 +23,121 @@ public class HWHelper extends CordovaPlugin {
     private static final String AMAZON_PLATFORM = "amazon-fireos";
     private static final String AMAZON_DEVICE = "Amazon";
 
+    private FirebaseAnalytics firebaseAnalytics;
+
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("coolMethod")) {
-            String message = args.getString(0);
-            this.coolMethod(message, callbackContext);
-            return true;
-        }
-        else if(action.equals("getDeviceInfo")) {
-            Log.i(TAG, "getDeviceInfo");
+        if(action.equals("getDeviceInfo")) {
+            Log.d(TAG, "getDeviceInfo");
             String message = args.getString(0);
             this.getDeviceInfo(message, callbackContext);
+            return true;
+        }
+        else if(action.equals("pluginInitialize")){
+            Log.d(TAG, "pluginInitialize");
+            this.pluginInitialize(callbackContext);
+            return true;
+        }
+        else if(action.equals("logEvent")){
+            Log.d(TAG, "logEvent");
+            String name = args.getString(0);
+            JSONObject params = args.getJSONObject(1);
+            this.logEvent(name, params, callbackContext);
+            return true;
+        }
+        else if(action.equals("setUserId")){
+            Log.d(TAG, "setUserId");
+            String userId = args.getString(0);
+            this.setUserId(userId, callbackContext);
+            return true;
+        }
+        else if(action.equals("setUserProperty")){
+            Log.d(TAG, "setUserProperty");
+            String name = args.getString(0);
+            String value = args.getString(1);
+            this.setUserProperty(name, value, callbackContext);
+            return true;
+        }
+        else if(action.equals("resetAnalyticsData")){
+            Log.d(TAG, "resetAnalyticsData");
+            this.resetAnalyticsData(callbackContext);
+            return true;
+        }
+        else if(action.equals("setEnabled")){
+            Log.d(TAG, "setEnabled");
+            boolean enabled = args.getBoolean(0);
+            this.setEnabled(enabled, callbackContext);
+            return true;
+        }
+        else if(action.equals("setCurrentScreen")){
+            Log.d(TAG, "setCurrentScreen");
+            String screenName = args.getString(0);
+            this.setCurrentScreen(screenName, callbackContext);
             return true;
         }
         return false;
     }
 
-    private void coolMethod(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) {
-            callbackContext.success(message);
-        } else {
-            callbackContext.error("Expected one non-empty string argument.");
+    private void pluginInitialize(CallbackContext callbackContext) {
+        Log.d(TAG, "Starting Firebase Analytics plugin");
+        Context context = this.cordova.getActivity().getApplicationContext();
+        this.firebaseAnalytics = FirebaseAnalytics.getInstance(context);
+        callbackContext.success("init success");
+    }
+    private void logEvent(String name, JSONObject params, CallbackContext callbackContext) {
+        Bundle bundle = new Bundle();
+        Iterator<String> it = params.keys();
+        try{
+            while (it.hasNext()) {
+                String key = it.next();
+                Object value = params.get(key);
+                if (value instanceof String) {
+                    bundle.putString(key, (String)value);
+                } else if (value instanceof Integer) {
+                    bundle.putInt(key, (Integer)value);
+                } else if (value instanceof Double) {
+                    bundle.putDouble(key, (Double)value);
+                } else if (value instanceof Long) {
+                    bundle.putLong(key, (Long)value);
+                } else {
+                    Log.w(TAG, "Value for key " + key + " not one of (String, Integer, Double, Long)");
+                }
+            }
+            this.firebaseAnalytics.logEvent(name, bundle);
+            callbackContext.success();
         }
+        catch(Exception e){
+            Log.d(TAG, Log.getStackTraceString(e));
+            callbackContext.error(e.getMessage());
+        }
+    }
+    private void setUserId(String userId, CallbackContext callbackContext) {
+        this.firebaseAnalytics.setUserId(userId);
+        callbackContext.success();
+    }
+    private void setUserProperty(String name, String value, CallbackContext callbackContext) {
+        this.firebaseAnalytics.setUserProperty(name, value);
+        callbackContext.success();
+    }
+    private void resetAnalyticsData(CallbackContext callbackContext) {
+        this.firebaseAnalytics.resetAnalyticsData();
+        callbackContext.success();
+    }
+    private void setEnabled(boolean enabled, CallbackContext callbackContext) {
+        this.firebaseAnalytics.setAnalyticsCollectionEnabled(enabled);
+        callbackContext.success();
+    }
+    private void setCurrentScreen(String screenName, CallbackContext callbackContext) {
+        firebaseAnalytics.setCurrentScreen(
+            cordova.getActivity(),
+            screenName,
+            null
+        );
+        callbackContext.success();
     }
 
     private void getDeviceInfo(String message, CallbackContext callbackContext){
+        Log.d(TAG, "Starting to get the device info");
         try{
             JSONObject r = new JSONObject();
             r.put("uuid", this.getUuid());
--- www/HWHelper.js
+++ www/HWHelper.js
@@ -3,3 +3,24 @@ var exec = require('cordova/exec');
 exports.getDeviceInfo = function(arg0, success, error){
     exec(success, error, 'HWHelper', 'getDeviceInfo', [arg0]);
 };
+exports.pluginInitialize = function(success, error){
+    exec(success, error, 'HWHelper', 'pluginInitialize');
+};
+exports.logEvent = function(name, params, success, error){
+    exec(success, error, 'HWHelper', 'logEvent',  [name, params || {}] );
+};
+exports.setUserId = function(userId, success, error){
+    exec(success, error, 'HWHelper', 'setUserId',  [userId] );
+};
+exports.setUserProperty = function(name, params, success, error){
+    exec(success, error, 'HWHelper', 'setUserProperty',  [name, params || {}] );
+};
+exports.resetAnalyticsData = function(success, error){
+    exec(success, error, 'HWHelper', 'resetAnalyticsData');
+};
+exports.setEnabled = function(enabled, success, error){
+    exec(success, error, 'HWHelper', 'setEnabled',  [enabled] );
+};
+exports.setCurrentScreen = function(screenName, success, error){
+    exec(success, error, 'HWHelper', 'setCurrentScreen',  [screenName] );
+};
